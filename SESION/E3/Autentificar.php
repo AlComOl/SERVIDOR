21d6b73d3a270ca5c0cccda4a1221f3deeca5d86
Alvaro <alvcomoli@alu.edu.gva.es>
2024-10-19 01:03:14 +0200

    bn

--- SESION/E3/Autentificar.php
+++ SESION/E3/Autentificar.php
@@ -1,24 +1,48 @@
 <?php
 session_start();
-// include("BD.php");
+include("BD.php");
+$comprobado = false;
+$_POST["username"];
+$_POST["password"];
 
-if(isset($_POST["entrar"])){//si se manda el formulario
-    if( empty ($_POST["username"])|| empty( $_POST["password"]) ){//si esta vacio el formulario
-        $_SESSION["vacio"]=true;//declaramos y llenamos con true
-        header("Location: ./Login.php");//redireccionamos a Login
 
+if (isset($_POST["entrar"])) { //si se manda el formulario
+    if (empty($_POST["username"]) || empty($_POST["password"])) { //si esta vacio el formulario
+        $_SESSION["vacio"] = true; //declaramos y llenamos con true
+        header("Location: ./Login.php"); //redireccionamos a Login
+
+    } elseif (isset($_POST["username"]) && isset($_POST["password"])) { //si tenemos los campos llenos
+        $_SESSION["logueado"] = true;
+
+        $i = 0;
+        while ($i < count($usuarios) && $comprobado == false) { //mientras la menor pos array && comprobado==false correra
+
+
+            //si encontramos usuario introducido es igual al del array , igaul con el password
+            if (($usuarios[$i]["usuario"] == $_POST["username"]) && ($usuarios[$i]["password"] == $_POST["password"])) {
+
+                $comprobado = true; //para salir del bucle 
+                $nombre = $usuarios[$i]["nombre"]; //guardaria el nombre
+
+
+            }
+
+
+            $i++; //la i tiene que estar detras para que ciente el primero
+
+
+            //
+
+            if ($comprobado) {
+                $_SESSION["nombre"] = $_usuarios[$i][""];
+                $_SESSION["password"] = $_usuarios[""];
+                $_SESSION["rool"] = $_usuarios[""];
+            }
+        }
     }
-    elseif(isset($_POST["username"]) && isset($_POST["password"])){//si tenemos los campos llenos
-        $_SESSION["logueado"]= true;
-        header("Location: ./BD.php");
 
-        while($_usuarios["usuario"]==$_POST["username"]){//hay que poner un buleano para que pare esta mal 
-
-            if($_usuarios["usuario"]==$_POST["username"] ){
-    }
-}
-
-}
+    if(isset($_SESSION["rool"])) {
+        //aqui redireccionare a menu para segun el tipo de usuario que soy 
 }
 // if(isset($_POST["username"])|| empty($_POST["username"])){
 
@@ -33,8 +57,3 @@ if(isset($_POST["entrar"])){//si se manda el formulario
 //     header("Location: ./Login.php");
 //     echo"VACIO";
 // }
-
-
-
-
-
