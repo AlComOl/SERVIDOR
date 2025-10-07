3293d198b15f061d17bc2e37da133945cf5695f8
Alvaro <alvcomoli@alu.edu.gva.es>
2025-10-07 18:04:47 +0200

    desde el bar

--- 3DAWEJERCICIOS/EJ_SINTAXIS/e11.php
+++ 3DAWEJERCICIOS/EJ_SINTAXIS/e11.php
@@ -16,20 +16,69 @@
 </html>
 <table>
 <?php
-$rgb=-50;
+$rgb="";
 $hexa=0;
+$r=$g=$b=0;
+$bandera=0;
+
+
+// while($bandera!==5){
+//     $bandera++;
+
+    $i=$b;
+    for($i=0;$i<250;$i+=50){
+      
+      if($g<250){
+                  $g=0;
+                  $rgb.=$g.",";
+                   
+      }else{
+                  $g+=50;
+                  $rgb.=$g.","; 
+           }
+
+        for ($x=0; $x<250; $x++) { 
+                if($b<250){
+                  $g=0;
+                  $rgb.=$g.",";
+                   
+                }else{
+                  $g+=50;
+                  $rgb.=$g.","; 
+                }
+            $rgb.=$g.",";
+           
+            for ($y=0; $y < 1; $y++) { 
+                if($b<250){
+                  $rgb.=$b.",";
+                  $b+=50; 
+                    }
+            
+                
+            }
+             echo"$rgb  <br>"; 
+              
+        }
+
+       
+      
+       
+    }
+
+
+// }
 
  
     
-    for ($i=0; $i < 36; $i++) { 
-        echo"<tr>";
-        for ($z=0; $z < 6; $z++) { 
+    // for ($i=0; $i < 36; $i++) { 
+    //     echo"<tr>";
+    //     for ($z=0; $z < 6; $z++) { 
         
-            $rgb+=50;
-        echo'<td>'.$hexa=dechex($rgb).'</td>';
-        }
-        echo"</tr>";
-    }
+    //         $rgb+=50;
+    //     echo'<td>'.$hexa=dechex($rgb).'</td>';
+    //     }
+    //     echo"</tr>";
+    // }
 
 ?>
 </table>
