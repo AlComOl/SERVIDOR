50912aedfc6f21e0e339b92cceec80268761ccd8
AlComOl <alcomoli@alu.edu.gva.es>
2025-10-08 13:54:23 +0200

    empezando de nuevo colores

--- 3DAWEJERCICIOS/EJ_SINTAXIS/e11.php
+++ 3DAWEJERCICIOS/EJ_SINTAXIS/e11.php
@@ -16,6 +16,29 @@
 </html>
 <table>
 <?php
+$r=0;
+$g=0;
+$b=0;
+$rgb=0;
+
+for($i=0; $i<=250; $i=$i+50) {
+  //$rgb.=$i.","; 
+
+  for ($z=0; $z<=250 ; $z=$z+50) { 
+    
+     //$rgb.=$z.","; 
+    
+     for ($y=0; $y <= 250; $y=$y+50) { 
+      //$rgb.=$y."<br>";
+       echo dechex($i)'($z).','.($y) <br>";
+      
+      
+    }
+  }
+}
+
+print_r($rgb);
+
 
 ?>
 </table>
