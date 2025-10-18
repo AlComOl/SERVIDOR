11f2a8676a48a41f1869709ebc85984ce7bff309
Alvaro <alvcomoli@alu.edu.gva.es>
2025-10-18 17:25:39 +0200

    al cumple de Olivia

---
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/r7.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    table{
+
+    }
+</style>
+<body>
+    
+</body>
+</html>
+
+<?php
+$tiendas=array("A");
+
+$tiendas1 = [
+
+    [
+        "Enero"=>1200,
+        "Marzo"=>200,
+        "Abril"=>4000
+    ],
+    [
+        "Enero"=>600,
+        "Marzo"=>300,
+        "Abril"=>700
+    ],
+    [
+        "Enero"=>900,
+        "Marzo"=>1200,
+        "Abril"=>5000
+    ],
+];
+
+?>
