f61ff4d214d1ebd010852d47c06ccc1b1822a5a9
Alvaro <alvcomoli@alu.edu.gva.es>
2025-10-19 12:48:05 +0200

    Ingresos tiendas falta poner la tienda que gasta mas

--- 3DAWEJERCICIOS/EJ_Sintaxis1/r7.php
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/r7.php
@@ -6,8 +6,14 @@
     <title>Document</title>
 </head>
 <style>
-    table{
+    table{  
+        width: 250px;
+        height: 250px;
 
+    }
+
+    tr,td,th{
+        text-align:center;
     }
 </style>
 <body>
@@ -16,7 +22,13 @@
 </html>
 
 <?php
-$tiendas=array("A");
+$tiendas=array(array("Enero"=>1200,"Marzo"=>200,"Abril"=>4000),
+          array("Enero"=>600,"Marzo"=>300,"Abril"=>700),
+          array("Enero"=>900,"Marzo"=>1200,"Abril"=>5000)
+        
+        );
+
+
 
 $tiendas1 = [
 
@@ -36,5 +48,35 @@ $tiendas1 = [
         "Abril"=>5000
     ],
 ];
+$monthEarn=0;
+$EarnArrayTotal=[];
+$highestMonthlyProfit=0;
+$maxKey=0;
+echo "<table>";
+echo"<th>Tiendas</th>";
+echo"<th>Enero</th>";
+echo"<th>Febrero</th>";
+echo"<th>Marzo</th>";
 
+foreach($tiendas as $mes => $ingreso){
+    echo"<tr>";
+    echo"$maxKey,$mes";
+    if($maxKey===$mes){
+          echo"<td><strong>$mes</strong></td>";
+    }else{
+          echo"<td>$mes</td>";
+    }
+  
+        foreach( $ingreso as $ingresos){
+            $monthEarn+=$ingresos;
+            echo"<td>$ingresos</td>";
+        }
+         array_push($EarnArrayTotal,$monthEarn);
+        $monthEarn=0;
+     echo"</tr>";
+}
+ $highestMonthlyProfit=max($EarnArrayTotal);
+ $maxKey=array_search($highestMonthlyProfit,$EarnArrayTotal);
+ print_r($maxKey);
+  
 ?>
