88797cfc9b8a83ce4bfb31064b6076082a5745db
AlComOl <alcomoli@alu.edu.gva.es>
2025-10-22 13:54:12 +0200

    mas cadenas substract

--- 3DAWEJERCICIOS/EJ_Sintaxis1/cadenas/c2.php
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/cadenas/c2.php
@@ -17,27 +17,36 @@ echo"<br>",
 // Letras totales y cantidad de palabras
 // Una línea por cada palabra indicando su tamaño
 // echad un vistazo a la función str_word_count
+//strlen -> longitud  
+//count -> cuenta logitud array
+$phrase = "Vamos a mostrar las letras totales de la frase"."<br>";
 
-$phrase = " Vamos a mostrar las letras totales de la frase "."<br>";
+$phraseArray=explode(" ",$phrase);//pasamos array con separación
 
+print_r($phraseArray);
 
-//quita los espacion 
-$cadenaSinEspacios=str_replace(" ", "",$phrase);
+$palabras=count($phraseArray);//contamos posiciones array (palabras)
+echo"<br><br>";
+for($i=0;$i<count($phraseArray);$i++){
+   echo strlen($phraseArray[$i]);//cuenta cada indice
+    echo "<br>";
+}
+//cuenta el numero de palabras de la frase, tiene que ser un string
+$vistazo=str_word_count($phrase);
 
-echo"Devuelve la frase sin espacion $cadenaSinEspacios";
+echo $vistazo;
+echo "<br>";
 
-//contar las palabras de la cadena 
-$count=0;
+//A partir de una frase, devuelve la cantidad de cada una de las vocales,
+//y el total de ellas. Hacedlo de forma tradicional y usando substr_count.
 
-$phrase1=trim($phrase);
+ 
+$cadena="hola que tal como estas";
 
-// echo"$phrase";
-// echo"$phrase1";
+for($i=0;$i<strlen($cadena);$i++){
 
-for($z=0;$z<strlen($phrase1);$z++){
-
-    $palabrasFrase=($phrase1[$z]===" ")?$count++:$count;
+    echo $cadena[$i]."->".substr_count($cadena,$cadena[$i])."<br>";
 
 }
-echo "devuelve el numero de palabras de la frase". ++$palabrasFrase;
+
 
