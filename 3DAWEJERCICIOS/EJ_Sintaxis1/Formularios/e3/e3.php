2a3af493108fc9f1a88168c475e98941947de5fc
AlComOl <alcomoli@alu.edu.gva.es>
2025-10-28 10:34:56 +0100

    clase

---
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/Formularios/e3/e3.php
@@ -0,0 +1,34 @@
+<?php
+//Formulario y Vectores*
+
+
+//Se desea realizar un formulario en el servidor que genere 10 inputs 
+//y un botón, a continuación el servidor recogerá todos los datos los 
+//mostrará y calculará la suma tota
+if(isset($_POST['enviar'])){
+    $sumar=0;
+    for($i=0;$i<8;$i++){
+        
+    }
+
+}
+
+
+
+for($i=0;$i<8;$i++){
+    echo" número[$i] <input name='n$i'></input><br>";
+}
+echo"<button type='submit' name='enviar'>Enviar</button><br>"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
