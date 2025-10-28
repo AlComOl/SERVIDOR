1cd1e125f7d33ea891edb47db044b0ecfc323d44
Alvaro <alvcomoli@alu.edu.gva.es>
2025-10-28 11:40:03 +0100

    php casa

--- 3DAWEJERCICIOS/EJ_Sintaxis1/Formularios/e3/e3.php
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/Formularios/e3/e3.php
@@ -5,26 +5,31 @@
 //Se desea realizar un formulario en el servidor que genere 10 inputs 
 //y un botón, a continuación el servidor recogerá todos los datos los 
 //mostrará y calculará la suma tota
-$numeros=$_POST["numero"];
+
+
+if(isset($_POST["enviar"])){
+$numeros=$_POST["numeros"];
+}
+$suma=0;
+
+
+for($i=0;$i<=9;$i++){
+    echo"<form method='post'>";
+    echo" número[$i] <input name='numeros[]'></input><br>";
+}
+echo"<button type='submit' name='enviar'>Enviar</button><br>";
+echo"</form>";
+
 
 if(isset($_POST['enviar'])){
-    
+
     for ($i=0; $i < count($numeros); $i++) { 
-        $suma+=$numeros[$i];
-     echo"$suma";
+        $suma= $suma+$numeros[$i];
+  
     }
 
 }
 echo"$suma";
-
-
-for($i=0;$i<8;$i++){
-    echo"<form >";
-    echo" número[$i] <input name='numero[]'></input><br>";
-}
-echo"<button type='submit' name='enviar'>Enviar</button><br>";
-echo"</form>";
-echo $suma;
 ?>
 
 <!DOCTYPE html>
