10d5822862f85d605fde08d388fc97998ea520c4
Alvaro <alvcomoli@alu.edu.gva.es>
2025-11-02 18:48:09 +0100

    subida archivos

--- 3DAWEJERCICIOS/EJ_Sintaxis1/Formularios/e4.1/index.php
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/Formularios/e4.1/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
     parte 1:
@@ -17,26 +17,19 @@
 parte2: **
 
 <p>Sería interesante que nos diera opción de poder eliminar las imágenes que hemos subido a nuestro servidor. Crearemos unos botones que cada uno de ellos eliminará la imagen que tiene a su lado.  Interesante mirar qué hace unlink</p><br>
+
+
 <form enctype="multipart/form-data" action="./server.php" method="POST">
     <!-- // MAX_FILE_SIZE debe preceder al campo de entrada del fichero  -->
     <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
     <!-- El nombre del elemento de entrada determina el nombre en el array $_FILES -->
-    <!-- //Enviar este fichero:  -->
+    Enviar este fichero: 
     <input name="fichero_usuario" type="file" />
     <input type="submit" name="boton" value="Enviar fichero" />
 </form>    
 </body>
 </html>
-<?php
 
-
-$carpeta = __DIR__ . '/subida/';
-$archivos = scandir($carpeta);
-
-print_r($archivos);
-
-
-?>
 
 
 
--- FORMULARIOS/E1_ENVIAR_RECIBIR/recibir.php
+++ FORMULARIOS/E1_ENVIAR_RECIBIR/recibir.php
@@ -2,27 +2,27 @@
 
 
 
-// print_r($_POST);
-// $nombre=$_GET["nombre"];
-// $apellido=$_GET["apellido"];
+print_r($_POST);
+$nombre=$_GET["nombre"];
+$apellido=$_GET["apellido"];
 
-// echo "nombre:".$nombre."apellido".$apellido;
-
-
-//print_r($_POST);
-
-// $lenguajes = $_POST["lenguajes"];
-
-// foreach ($lenguajes as $lenguaje) {
-//     echo "$lenguaje <br />";
-// }
+echo "nombre:".$nombre."apellido".$apellido;
 
 
-// $cursos = $_POST["curso"];
+print_r($_POST);
 
-// foreach ($cursos as $curso) {
-//     echo "$curso <br />";
-// }
+$lenguajes = $_POST["lenguajes"];
+
+foreach ($lenguajes as $lenguaje) {
+    echo "$lenguaje <br />";
+}
+
+
+$cursos = $_POST["curso"];
+
+foreach ($cursos as $curso) {
+    echo "$curso <br />";
+}
 
 
 
