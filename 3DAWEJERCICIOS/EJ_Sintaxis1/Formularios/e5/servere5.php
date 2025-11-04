541941205041e1c8b836a61fd1a4f74edd4fa809
Alvaro <alvcomoli@alu.edu.gva.es>
2025-11-04 16:16:01 +0100

    parece que ya funciona

--- 3DAWEJERCICIOS/EJ_Sintaxis1/Formularios/e5/servere5.php
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/Formularios/e5/servere5.php
@@ -1,33 +1,36 @@
 <?php
-$dir_subida = './subir/';
-$fichero_subido = $dir_subida . basename($_FILES['fichero_usuario']['name']);
-
-//basename: muestra el nombre del fichero con la extensión
-
-echo '<pre>';
 if(isset($_POST["Enviar"])){
-   
+    $dir_subida = './subir/';
+    $fichero_subido = $dir_subida . basename($_FILES['fichero_usuario']['name']);
 
-    if(move_uploaded_file($_FILES['fichero_usuario']['tmp_name'], $fichero_subido)) {
-        echo "El fichero es válido y se subió con éxito.\n";
-    } else {
-        echo "¡Posible ataque de subida de ficheros!\n";
-    }
 
-    // echo 'Más información de depuración:';
-    // print_r($_FILES);
+    //basename: muestra el nombre del fichero con la extensión
+
+    echo '<pre>';
+
+    
+
+        if(move_uploaded_file($_FILES['fichero_usuario']['tmp_name'], $fichero_subido)) {
+            echo "El fichero es válido y se subió con éxito.\n";
+        } else {
+            echo "¡Posible ataque de subida de ficheros!\n";
+        }
+
+        // echo 'Más información de depuración:';
+        // print_r($_FILES);
 
     
     
 }else if(isset($_POST["eliminar"])){
-       $posicion=$_POST["eliminar"];
-       $archivo=scandir($dir_subida);//el scandir lo recorre y mete dentro del array nuevo
-       $newArchivo=array_slice($archivo,2);
-       $ruta="./subir/".$newArchivo[$posicion];
-       unlink($ruta);
-  
 
-print "</pre>";
+    $posicion=$_POST["eliminar"];
+
+    $archivo=array_slice(scandir("./subir"),2);//el scandir lo recorre y mete dentro del array nuevo
+    $ruta="./subir/$archivo[$posicion]";
+
+    unlink($ruta);
+
+    print "</pre>";
 
 
 }
@@ -54,13 +57,8 @@ print "</pre>";
 
 <?php
 
- $newArchivo=array_slice(scandir("./subir/",2));
+$archivo=array_slice(scandir("./subir"),2);
 
- foreach ($newArchivo as $key => $value) {
-  echo"<form method='POST'><img src='./$value'>$key  <button type='submit' name='eliminar' id=''value='$value'>Eliminar</button></form>";
+ foreach ($archivo as $key => $value) {
+  echo"<form method='POST'><img src='./$value'>$key  <button type='submit' name='eliminar' id=''value='$key'>Eliminar</button></form>";
 }
-
-
-
-
-
