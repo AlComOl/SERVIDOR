acae434a9bfed5cca96f2ffbf6c83834af116511
Alvaro <alvcomoli@alu.edu.gva.es>
2025-11-11 20:26:53 +0100

    terminado 1 carrito

--- 3DAWEJERCICIOS/EJ_Sintaxis1/Cockies&Sesion/carrito/index.php
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/Cockies&Sesion/carrito/index.php
@@ -1,5 +1,65 @@
+<?php
+session_start();
+
+if(isset($_POST['Anadir'])){
+
+   
+        $producto=$_POST['producto'];
+        $quantity=$_POST['quantity'];
 
 
+        if(!isset($_SESSION['cesta'])){
+
+            $_SESSION["cesta"][$producto]=$quantity;
+
+        }else{
+
+            if(isset($_SESSION['cesta'][$producto])){
+
+                  $_SESSION['cesta'][$producto]+=$quantity;
+
+            }else{
+
+                  $_SESSION["cesta"][$producto]=$quantity;
+
+            }
+        }
+
+    }
+
+     if(isset($_POST['borrar'])){
+
+        $index=$_POST['borrar'];
+
+        unset($_SESSION['cesta'][$index]);
+     }
+
+      if(isset($_POST['reset'])){
+        $index=$_POST['reset'];
+        session_destroy();
+       
+        
+     }
+
+     if(isset($_SESSION['cesta'])){
+
+            foreach ($_SESSION['cesta'] as $key => $value) {
+
+                echo"<form method='post'><h3> $key y cantidad $value </h3><button type='submit' name='borrar' value='$key'> Eliminar </button></form>";
+            }
+
+             print_r($_SESSION["cesta"]);
+    }
+      
+   
+
+
+
+   
+
+
+        
+    ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +69,7 @@
     <title>Carrito Compra</title>
 </head>
 <body>
-<h3>CARRITO DE LA COMPRA |</h3>
+<h3>CARRITO DE LA COMPRA</h3>
     <form method="post">
         Producto:<input type="text" name="producto" id="">
         Cantidad:<input type="number" id="quantity" name="quantity" min="1" max="15">
@@ -17,69 +77,10 @@
         <button type="submit" name="reset">Reset</button>
     </form>
 
+        
+</body>
+</html>
+
     
     
 <?php
-
-
-if(isset($_POST['Anadir'])){
-
-   
-        $producto=$_POST['producto'];
-        $quantity=$_POST['quantity'];
-
-    if(isset( $_SESSION["cesta"])){
-
-        //comprobar si existe el producto y añadorlo de otra manera para no machacar el array
-
-            $_SESSION['cesta'][$producto]
-
-        // $_SESSION["cesta"][$producto]=$quantity;
-
-        print_r($_SESSION['cesta']);
-
-    }else{
-
-        $_SESSION["cesta"][$producto]=$quantity;
-
-    }
-        // foreach($productosArray as $key => $value){
-        //     if($key['producto']===$producto){
-        //         $value+=$quantity;
-        //     }
-        }
-
-
-
-
-            // $productosArray['producto']=$producto;
-
-            // $productosArray['quantity']=$quantity;
-
-
-
-        // print_r($productosArray);
-
-        else{
-            // $productosArray=[];
-
-            //     $producto=$_POST['producto'];
-            //     $quantity=$_POST['quantity'];
-
-
-            //     $productosArray['producto']=$producto;
-
-            //         $productosArray['quantity']=$quantity;
-
-
-                
-        }
-
-
-
-
-        
-    ?>
-    
-</body>
-</html>
