264cd833fd47f1a969c0f2d5c3451d4a207d8a12
Alvaro <alvcomoli@alu.edu.gva.es>
2025-11-18 12:05:53 +0100

    juego frutas

--- 3DAWEJERCICIOS/EJ_Sintaxis1/CarritoNew/carrito.php
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/CarritoNew/carrito.php
@@ -3,7 +3,18 @@ session_start();
 
 if(!isset($_SESSION['productos'])){
         $_SESSION['productos']=[];
-        }
+}
+
+if(isset($_POST['eliminar'])){
+
+}
+
+if(isset($_POST['borrar'])){
+
+    session_destroy();
+}
+
+
 
 if(isset($_POST['nombre']) && isset($_POST['cantidad'])){
 
@@ -15,8 +26,6 @@ if(isset($_POST['nombre']) && isset($_POST['cantidad'])){
     if(isset($_SESSION['productos'])){
                     
         array_push($_SESSION['productos'],['nombre'=> $nombre ,'cantidad'=> $cantidad]);
-
-        
             
     }
    
@@ -60,13 +69,30 @@ if(isset($_POST['nombre']) && isset($_POST['cantidad'])){
         <form  method="post">
             <h2>Nombre del Producto:</h2><input type="text" name="nombre" value="">
             <h2>Cantidad:</h2><input type="number" name="cantidad" value="">
-            <button type="submit">Agregar</button ><button type="submit" >Borrar</button>
+            <button type="submit" >Agregar</button ><button type="submit" name="borrar" >Borrar</button>
         </form>
     </div>
 
     <div>
         <h3><strong>Carrito</strong></h3>
-        <?php if(isset($_SESSION['productos'])){ echo print_r($_SESSION['productos']); }else{ echo"El carrito esta vacio";} ?>
+        <?php 
+        
+        
+        if(isset($_SESSION['productos'])){
+
+                    foreach ($_SESSION['productos'] as $key => $value) {
+
+                       echo"<h3>Producto: {$value['nombre']} con una cantidad de : {$value['cantidad']} <form method='post'> <button style=' height: 30px; width: 50px;' name='eliminar' value='$key'>Eliminar</button> </form> <h3>";
+                    }
+            if(isset($_POST['eliminar'])){
+             
+
+                unset($_SESSION['productos'] [$_POST['eliminar']]);
+            }
+               
+
+            }else{
+                 echo"El carrito esta vacio";} ?>
     </div>
 
 
