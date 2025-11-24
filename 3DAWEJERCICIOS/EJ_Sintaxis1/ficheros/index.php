a93d6fae4c81f01d74d993ed21e05666b645a385
AlComOl <alcomoli@alu.edu.gva.es>
2025-11-24 09:51:25 +0100

    clase

---
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/ficheros/index.php
@@ -0,0 +1,9 @@
+<!-- Se desea crear una miniaplicación de registro de notas.
+
+    Existirá un formulario donde se almacenarán en formato .csv el dni, nota media y ciclo del alumno. Por ejemplo, "20021225w, 8, DAW".
+
+20021225W, 8, DAW
+
+12548754C, 7, ASIR
+
+45785415Z, 5, SMR -->
---
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/funciones/e2.php
@@ -0,0 +1,92 @@
+<?php
+
+// Crea las siguientes funciones:
+
+// Una función que averigüe si un número es par: esPar(int $num): bool
+
+function esPar(int $num){
+    if($num%2==0){
+        return "es par";
+    }else{
+        return "no es par";
+    }
+}
+
+
+print(esPar(2));
+
+echo"<br>";
+
+// Una función que devuelva un array de tamaño $tam con números aleatorios comprendido entre
+//  $min y $max : arrayAleatorio(int $tam, int $min, int $max) : array
+
+
+function tamano(int $tam, int $min,int $max){
+    $myarray=[];
+
+    for($i=0;$i<$tam;$i++){
+        $num=rand($min,$max);
+        array_push($myarray,$num);
+    }
+
+    return $myarray;
+
+}
+
+print_r(tamano(5,2,10));
+
+echo"<br>";
+
+// Una función que reciba un $array por referencia y devuelva la cantidad 
+// de números pares que hay almacenados: arrayPares(array &$array): int
+
+function numPares(array &$array){
+    $count=0;
+    $tam=count($array);
+    for ($i=0; $i < $tam; $i++) { 
+
+        if($array[$i]%2==0){
+            $count++;
+        }
+        
+    }
+    $array1=[];
+    array_push($array1,$count);
+    return $array1;
+}
+
+
+$array2=[2,3,5,6,7,8,89,8,56,3,23];
+
+print_r(numPares($array2));
+
+
+// Crea las siguientes funciones:
+// Una función que devuelva el mayor de todos los números recibidos como 
+// parámetros: function mayor(): int. Utiliza las funciones func_get_args(),
+// etc... No puedes usar la función max().
+
+function mayor(){
+    if(func_get_arg()>)
+
+}
+1️⃣ Declarar la función
+
+Según tus apuntes, la firma sería:
+echo mayor(4, 7, 2, 9, 1);  // debería imprimir 9
+echo mayor(4, 7, 2, 9, 1);  // debería imprimir 9
+return $mayor;
+$mayor = $arrayArgs[0];  // inicializamos con el primero
+for ($i = 1; $i < $cantidad; $i++) {
+//     // comparar $arrayArgs[$i] con $mayor
+//     // si $arrayArgs[$i] > $mayor, actualizar $mayor
+// }
+// Inicializar una variable para el mayor
+
+// Piensa así: para saber cuál es el mayor, podemos:
+
+// Tomar como referencia el primer elemento de $arrayArgs como candidato inicial al mayor.
+
+// Luego recorrer los demás y comparar: si encontramos uno mayor, lo guardamos como nuevo mayor.
+
+// Ejemplo de idea conceptual:
