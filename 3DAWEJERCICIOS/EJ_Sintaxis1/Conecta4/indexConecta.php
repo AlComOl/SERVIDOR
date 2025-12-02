df1e267061ff2e8f408ce6322608d89b406bb157
AlComOl <alcomoli@alu.edu.gva.es>
2025-12-02 10:46:11 +0100

    clase

---
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/Conecta4/indexConecta.php
@@ -0,0 +1,35 @@
+<?php
+include("Location:./conectaLogica.php");
+
+echo"<table>";
+for ($i=0; $i < 6 ; $i++) {
+    echo"<tr>";
+
+    for ($i=0; $i < 6; $i++) { 
+        echo"<td><td>";
+    }
+}
+echo"</table>"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    table{
+
+    }
+
+    tr,td{
+        width: 20px;
+        height: 30px;
+        border: 2px solid black;
+    }
+</style>
+<body>
+    
+</body>
+</html>
---
+++ 3DAWEJERCICIOS/EJ_Sintaxis1/Conecta4/logicaConecta.php
@@ -0,0 +1,20 @@
+<?php
+class Conecta{
+ 
+    private $juegoConecta=[[],[],[],[],[],[]];
+    private $jugador=rand(1,2);
+
+    public function __constructor(){
+
+        $this->$juegoConecta;
+        $this->$jugador;
+
+    }
+
+
+}
+
+//insetar - columna
+
+//
+?>
